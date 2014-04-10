60d7585d4865a45d967d70c9de7b8bc65692337a
nphuc <shyboykt@yahoo.com>
2014-04-10 20:17:27 +0700

    fix error printNumber.c

--- printNumber.c
+++ printNumber.c
@@ -2,6 +2,6 @@
         char __p[21];\
         register char __plen=0;\
         register unsigned long long __n=(n);\
-        while(__n){ __p[__plen++]=__n%10; __n/=10;}\
+        while(__n||!__plen){ __p[__plen++]=__n%10; __n/=10;}\
         while(__plen--){putchar_unlocked('0'+__p[__plen]);}\
     }while(0)
