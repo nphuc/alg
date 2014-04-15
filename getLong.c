74713d947c2ac4e1074e31d3cf687da07d882091
nphuc <nguyenngocphuc@outlook.com>
2014-04-15 05:18:57 -0700

    fix getLong.c

--- getLong.c
+++ getLong.c
@@ -1,13 +1,15 @@
 long getLong(){
     long res=0;
     char c;
+    int flag=1;
     for(;;){
         c=getchar_unlocked();
         if(isdigit(c)) break;
+        else if(c=='-') flag=-1;
     }
     while(isdigit(c)){
         res=res*10+c-'0';
         c=getchar_unlocked();
     }
-    return res;
+    return res*flag;
 }
